61ebdaa85f7c94320da726870009d0140d79577f
Jeppe Hansen <au591132@uni.au.dk>
2022-03-28 16:27:05 +0200

    Buyer + IBuyer implementation added

---
+++ AuctionHouseClassLibrary/Buyer.cs
@@ -0,0 +1,52 @@
+﻿
+
+using AuctionHouseClassLibrary.Interfaces;
+
+namespace AuctionHouseClassLibrary
+{
+    public class Buyer : IBuyer
+    {
+
+        public int accountBalance { get; set; }
+        public int bid { get; set; }
+        public void GiveBid()
+        {
+
+            string userInput;
+            int intVal;
+
+            Console.WriteLine("Give bid");
+            userInput = Console.ReadLine();
+            intVal = Convert.ToInt32(userInput);
+            
+            
+
+            if (accountBalance >= intVal)
+            {
+                this.bid = intVal;
+                Console.WriteLine("Bid given: " + bid);
+            }
+            else
+            {
+                Console.WriteLine("Not enough money on account - Account: " + accountBalance + "\nBid exceedes account balance - Bid: " + intVal);
+            }
+
+
+        }
+
+        public void SetAccountBalance()
+        {
+            string userInputAccountBalance;
+            int intValAccountBalance;
+
+            Console.WriteLine("Enter amount on your account: ");
+            userInputAccountBalance = Console.ReadLine();
+            intValAccountBalance = Convert.ToInt32(userInputAccountBalance);
+            Console.WriteLine("AccountBalance: " + intValAccountBalance);
+
+            accountBalance = intValAccountBalance;
+        }
+
+        
+    }
+}
---
+++ AuctionHouseClassLibrary/Interfaces/IBuyer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouseClassLibrary.Interfaces
+{
+    public interface IBuyer
+    {
+        void SetAccountBalance();
+        void GiveBid();
+    }
+}
