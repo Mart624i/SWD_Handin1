06f2e83eb3c3e16f2d8aeb59165cc4e1ac9c7dac
Mart624i <martinahlmanpedersen@gmail.com>
2022-03-28 15:22:32 +0200

    Added Item interface and class.

--- AuctionHouseClassLibrary/Interfaces/IItem.cs
+++ AuctionHouseClassLibrary/Interfaces/IItem.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AuctionHouseClassLibrary
+namespace AuctionHouseClassLibrary.Interfaces
 {
-    internal interface Mediator
+    internal interface IItem
     {
-
+        
     }
 }
---
+++ AuctionHouseClassLibrary/Item.cs
@@ -0,0 +1,27 @@
+﻿using AuctionHouseClassLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouseClassLibrary
+{
+    public class Item : IItem
+    {
+        public string name { get; set; } = null!;
+        public double price { get; set; } 
+
+        public Item(string Name, double Price)
+        {
+            if(name != null || name != "")
+            {
+                name = Name;
+            }
+            if (price > 0)
+            {
+                price = Price;
+            }
+        }
+    }
+}
